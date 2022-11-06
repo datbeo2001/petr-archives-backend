b3f43ebb4a2cc03f0d65e0c883d4be19c28338e6
datboi2001 <manhn3@uci.edu>
2022-11-05 23:32:04 -0700

    Replaced fastapi.Settings with load_dotenv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from fastapi import FastAPI, UploadFile, Body, Form, HTTPException, File
 import uvicorn
 from posts import Sticker
-from uploadCloudinary import settings, cloudinaryUpload
+from uploadCloudinary import cloudinaryUpload
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -15,9 +15,11 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 @app.get("/")
 def home():
     return {}
+
 
 @app.get("/get-data")
 def data():
@@ -26,30 +28,29 @@ def data():
     file.close()
     return JSONdata
 
+
 def write_json(new_data, filename='data.json'):
     with open('data.json', 'r+') as file:
         file_data = json.load(file)
         file_data.append(new_data)
         file.seek(0)
-        json.dump(file_data, file, indent = 4)
+        json.dump(file_data, file, indent=4)
+
 
 @app.post("/create-sticker")
-def create_new_sticker(file: UploadFile = File(...), name: str = Form(""), timestamp: str = Form(""), 
-     location: str = Form(""), instagram: str = Form("")):
+def create_new_sticker(file: UploadFile = File(...), name: str = Form(""), timestamp: str = Form(""),
+                       location: str = Form(""), instagram: str = Form("")):
     try:
         contentType = file.content_type
         contents = file.file.read()
         url = cloudinaryUpload(contents, contentType)
-        stickerDict = {"name": name.title(), "image_link": url, "timestamp": timestamp, "location": location.title(), "instagram": f"http://www.instagram.com/{instagram}"}
+        stickerDict = {"name": name.title(), "image_link": url, "timestamp": timestamp, "location": location.title(),
+                       "instagram": f"http://www.instagram.com/{instagram}"}
         write_json(stickerDict)
         return {"status": "OK"}
-    except Exception as E: 
+    except Exception as E:
         raise E
 
 
-
-
-
 if __name__ == '__main__':
-    uvicorn.run('api:app', reload=True)
-
+    uvicorn.run('main:app', reload=True)
--- uploadCloudinary.py
+++ uploadCloudinary.py
@@ -1,29 +1,32 @@
-# import cloudinary
-# import cloudinary.uploader
 import cloudinary
 from pydantic import BaseSettings
 from PIL import Image
 import io
+from dotenv import load_dotenv, find_dotenv
 
-class Settings(BaseSettings):
-
-    cloud_name = "CLOUD_NAME"
-    api_key = "API_KEY"
-    api_sec = "API_SECRET"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
-
+# class Settings(BaseSettings):
+#
+#     cloud_name = "CLOUD_NAME"
+#     api_key = "API_KEY"
+#     api_secret = "API_SECRET"
+#
+#     class Config:
+#         env_file = ".env"
+#
+#
+# settings = Settings()
 
 import cloudinary.uploader
 import cloudinary.api
 
+load_dotenv(find_dotenv())
+
+
 def cloudinaryUpload(img: bytes | str, contentType: str):
+    file_name = f"something.{contentType.split('/')[1]}"
     if type(img) is bytes:
         file = Image.open(io.BytesIO(img))
-        file.save("something.jpeg")
-    res = cloudinary.uploader.upload(r"something.jpeg")
+        file.save(file_name)
+    res = cloudinary.uploader.upload(file_name)
     url = res["url"]
     return url
